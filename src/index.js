f36f22321cdb95c0120552f22cf9f93f2d9c2343
Nihil <carite.sapientis@ya.ru>
2022-01-04 23:35:40 +0300

    feat: tests passed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-const upToTwenty = ['', "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
-	"twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-const tens = ['', "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-const thousands = ['', 'thousand', 'million', 'billion'];
 module.exports = function toReadable(number) {
+const upToTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+	'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+const thousands = ['', 'thousand', 'million', 'billion'];
 
 	if (number === 0) {
-		return 0;
+		return 'zero';
 	}
 	let counter = 0;
 	let result = '';
 	while (number > 0) {
 		if (number % 1000 !== 0) {
-			result = `${getWord(number % 1000)} ${thousands[counter]} ${result}`;
+			result = `${getWord(number % 1000)} ${thousands[counter]}${result}`;
 		}
 		number = Math.floor(number / 1000);
 		counter += 1;
 	}
-	return result;
+	return result.trim();
 
 
 	function getWord(number) {
 		if (number === 0) {
-			return 'Zero';
+			return '';
 		}
 		else if (number < 20) {
 			return upToTwenty[number];
